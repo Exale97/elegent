8e23878e6956433479327f8061257cdb52d62170
user <185206@example.com>
2025-03-24 14:38:03 +0800

    hhhh

--- app/src/main/java/com/example/myapplication/util/MainActivity.java
+++ app/src/main/java/com/example/myapplication/util/MainActivity.java
@@ -1,12 +1,16 @@
-package com.example.myapplication;
+package com.example.myapplication.util;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,10 +25,11 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        Button button = findViewById(R.id.jjj);
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
 
-        // jaijfoajoajjaojfa
-        // afdfagag
-
-        // ajojofjafiow
     }
 }
